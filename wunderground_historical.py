7be0225caa889dab764e579bcb359ff188f573f9
Rachel <rachel.chloe.gibbs@gmail.com>
2017-11-21 14:35:26 -0600

    Update wunderground_historical.py

--- wunderground_historical.py
+++ wunderground_historical.py
@@ -62,6 +62,19 @@ while True:
 				pressure_wu = conditions['history']['observations'][i]['pressurem']
 				precip_wu = conditions['history']['observations'][i]['precipi']
 				wind_speed_wu = conditions['history']['observations'][i]['wspdi']
+				# dewpt_wu = conditions['history']['observations'][i]['dewpti']
+				# wind_gust_wu = conditions['history']['observations'][i]['wgusti']
+				# wind_dir_wu = conditions['history']['observations'][i]['wdird']
+				# vis_wu = conditions['history']['observations'][i]['visi']
+				# wind_chill_wu = conditions['history']['observations'][i]['windchilli']
+				# heat_index_wu = conditions['history']['observations'][i]['heatindexi']
+				# conds_wu = conditions['history']['observations'][i]['conds']
+				# fog_wu = conditions['history']['observations'][i]['fog']
+				# rain_wu = conditions['history']['observations'][i]['rain']
+				# snow_wu = conditions['history']['observations'][i]['snow']
+				# hail_wu = conditions['history']['observations'][i]['hail']
+				# thunder_wu = conditions['history']['observations'][i]['thunder']
+				# tornado_wu = conditions['history']['observations'][i]['tornado']
 
 				if isFloat(humidity_wu):
 					streamer.log("WU Humidity (%)",humidity_wu,epoch)
@@ -73,7 +86,33 @@ while True:
 					streamer.log("WU Rain",precip_wu,epoch)
 				if isFloat(wind_speed_wu):
 					streamer.log("WU Wind Speed",wind_speed_wu,epoch)
-		
+				# if isFloat(dewpt_wu):
+				# 	streamer.log("WU Dew Point",dewpt_wu,epoch)
+				# if isFloat(wind_gust_wu):
+				# 	streamer.log("WU Wind Gust",wind_gust_wu,epoch)
+				# if isFloat(wind_dir_wu):
+				# 	streamer.log("WU Wind Direction",wind_dir_wu,epoch)
+				# if isFloat(vis_wu):
+				# 	streamer.log("WU Visibility",vis_wu,epoch)
+				# if isFloat(wind_chill_wu):
+				# 	streamer.log("WU Wind Chill",wind_chill_wu,epoch)
+				# if isFloat(heat_index_wu):
+				# 	streamer.log("WU Heat Index",heat_index_wu,epoch)
+				# if isFloat(conds_wu):
+				# 	streamer.log("WU Conditions",conds_wu,epoch)
+				# if isFloat(fog_wu):
+				# 	streamer.log("WU Fog",fog_wu,epoch)
+				# if isFloat(rain_wu):
+				# 	streamer.log("WU Rain",rain_wu,epoch)
+				# if isFloat(snow_wu):
+				# 	streamer.log("WU Snow",snow_wu,epoch)
+				# if isFloat(hail_wu):
+				# 	streamer.log("WU Hail",hail_wu,epoch)
+				# if isFloat(thunder_wu):
+				# 	streamer.log("WU Thunder",thunder_wu,epoch)
+				# if isFloat(tornado_wu):
+				# 	streamer.log("WU Tornado",tornado_wu,epoch)
+
 				streamer.flush()
 				time.sleep(SECONDS_BETWEEN_SEND)
 
